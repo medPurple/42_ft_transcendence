9a42680d1515ee26d856192411b21bcb3f052e99
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2024-02-28 16:31:41 +0100

    feat: add fields class user

--- services/backend/user/user/authentication/models.py
+++ services/backend/user/user/authentication/models.py
@@ -6,9 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class	User(AbstractUser):
 	profile_picture = models.ImageField(null=True, blank=True)
 	name = models.CharField(max_length=200, null=True)
+	password = models.CharField(max_length=200, null=True)
 	phone = models.CharField(max_length=200, null=True)
 	email = models.CharField(max_length=200, null=True)
-	password = models.CharField(max_length=200, null=True)
+	token = models.CharField(max_length=200, null=True, blank=True)
 
 
 # Create your models here.
