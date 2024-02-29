e71730120dbb2ef20a1419767b764d4a96de41c7
ML <vautrot.marielaure@gmail.com>
2024-02-29 20:23:50 +0100

    feat: migrate userprofil

--- services/backend/user/user/profil/models.py
+++ services/backend/user/user/profil/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class UserProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
     profile_picture = models.ImageField(null=True, blank=True, upload_to='images')
+    bio = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
