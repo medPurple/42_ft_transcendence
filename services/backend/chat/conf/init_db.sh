37c55e8893ae9018bf517d20dc75acee78b9a7dd
Alban TARDIF <atardif@paul-f4Ar5s6.clusters.42paris.fr>
2024-02-29 16:23:54 +0100

    refactor: template creation for django containers

--- services/backend/chat/conf/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-su postgres <<EOF
-service postgresql start
-sleep 5
-
-psql -c "CREATE USER ${CHAT_DB_USER} WITH PASSWORD '${CHAT_DB_PASSWORD}';"
-psql -c "CREATE DATABASE ${CHAT_DB_BASENAME} OWNER POSTGRES;"
-EOF
--- services/backend/game/conf/init_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-su postgres <<EOF
-service postgresql start
-sleep 5
-
-psql -c "CREATE USER ${GAME_DB_USER} WITH PASSWORD '${GAME_DB_PASSWORD}';"
-psql -c "CREATE DATABASE ${GAME_DB_BASENAME} OWNER POSTGRES;"
-EOF
