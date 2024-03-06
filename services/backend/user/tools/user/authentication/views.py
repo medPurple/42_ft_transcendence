1a7bcf8ead064c3aaa33aa7692cdd0751cf12ea5
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2024-03-06 13:03:45 +0100

    feat: add token to user

--- services/backend/user/tools/user/authentication/views.py
+++ services/backend/user/tools/user/authentication/views.py
@@ -2,14 +2,22 @@ from django.shortcuts import render, redirect
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+import requests
+from django.contrib import messages 
+from django.http import HttpResponse
 
 def signup(request):
 	if request.method == 'POST':
 		form = CustomUserCreationForm(request.POST)
 		if form.is_valid():
 			#verify picture
-			user = form.save()
-			return redirect('login')
+			user = form.save(commit=False)
+			user.token = user_token(request)
+			if user.token is not None:
+				user.save()
+				return redirect('login')
+			else:
+				messages.error(request, 'Unable to retrieve a valid token. Please try again later.')
 	else:
 		form = CustomUserCreationForm()
 	return render(request, 'authentication/signup.html', {'form': form})
@@ -27,6 +35,19 @@ def user_login(request):
 	return render(request,
 		'authentication/login.html',
 		context = {'message': message})
+
+
+def user_token(request):
+    token_service_url = 'http://127.0.0.1/api/token_generate/'
+    try:
+        token_response = requests.post(token_service_url)
+        token_response.raise_for_status()  # Cette ligne lèvera une exception si la réponse HTTP n'est pas OK (200)
+
+        user_token = token_response.json().get('token')
+        return user_token
+    except requests.exceptions.RequestException as e:
+        print(f"Erreur lors de la connexion au service de génération de jetons : {e}")
+        return None
 
 
 def home(request):
