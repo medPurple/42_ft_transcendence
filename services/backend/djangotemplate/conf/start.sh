0c69b5eaf04a15e9039bcce7c8b430b62237bea7
Alban TARDIF <atardif@paul-f4Ar5s3.clusters.42paris.fr>
2024-03-14 15:10:59 +0100

    feat: implementing persisting database

--- services/backend/djangotemplate/conf/start.sh
+++ services/backend/djangotemplate/conf/start.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+#chmod -R 755 /var/lib/postgresql/
+
 service postgresql start
 
-sleep 2
+sleep 5
 
+# python3 manage.py makemigrations authentication
+#
+# sleep 5
+#
+# python3 manage.py migrate
+#
+# sleep 5
 python3 manage.py runserver 0.0.0.0:8080
