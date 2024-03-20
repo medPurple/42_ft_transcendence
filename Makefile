70fd22a7ec73fd290faa0784e5f5ef8241fe93ae
purple <purple.pro@gmail.com>
2024-03-20 15:56:48 +0100

    a

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 all:
+	@ docker network create -d bridge microservices
+	@ docker build -t vault services/vault/
+	@ docker run --name vault --network microservices -p 8200:8200 -v secret_volume:/vault/file --cap-add IPC_LOCK -e VAULT_ADDR=http://127.0.0.1:8200  -d vault
 	@ docker compose -f ./services/docker-compose.yml up -d --build
 
 down:
@@ -8,10 +11,14 @@ re:	down
 		@ docker compose -f ./services/docker-compose.yml up -d --build
 
 clean:
-	@ docker stop $$(docker ps -qa);\
+	@docker stop $$(docker ps -qa);\
 	 docker rm $$(docker ps -qa);\
 	 docker rmi -f $$(docker images -qa);\
 	 docker volume rm $$(docker volume ls -q);\
 	 docker network rm $$(docker network ls -q);\
 
+check_network:
+    if ! docker network inspect microservices &> /dev/null; then \
+        docker network create -d bridge microservices; \
+    fi
 .PHONY: all re down clean
---
+++ services/vault/init_vault.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+if [ ! -f /vault/file/vault_init.txt ]; then
+
+    echo "[VAULT] Starting server"
+    vault server -config=/vault/vault.json &
+    sleep 2
+    echo "[VAULT] Initialisation"
+    vault operator init -n 1 -t 1 > /vault/file/vault_init.txt
+    
+    sleep 1
+
+    echo "[VAULT] Key recuperation"
+    UNSEAL_KEY=$(awk '/^Unseal Key 1:/{print $NF}' /vault/file/vault_init.txt)
+    ROOT_TOKEN=$(awk '/^Initial Root Token:/{print $NF}' /vault/file/vault_init.txt)
+    echo "Unseal Key 1: $UNSEAL_KEY"
+    echo "Initial Root Token: $ROOT_TOKEN"
+    
+    vault operator unseal "$UNSEAL_KEY"
+    vault login "$ROOT_TOKEN"
+    sleep infinity
+else
+    echo "[VAULT] Starting server"
+    vault server -config=/vault/vault.json &
+    sleep 3
+    echo "[VAULT] Key recuperation"
+
+    UNSEAL_KEY=$(awk '/^Unseal Key 1:/{print $NF}' /vault/file/vault_init.txt)
+    ROOT_TOKEN=$(awk '/^Initial Root Token:/{print $NF}' /vault/file/vault_init.txt)
+    echo "Unseal Key 1: $UNSEAL_KEY"
+    echo "Initial Root Token: $ROOT_TOKEN"
+    
+    vault operator unseal "$UNSEAL_KEY"
+    vault login "$ROOT_TOKEN"
+    sleep infinity
+
+fi
