b175df7b3476514e276ef4358fd0e1d885226080
purple <purple.pro@gmail.com>
2024-03-21 19:57:57 +0100

    request for nothing, but good path

--- services/backend/user/conf/init_db.sh
+++ services/backend/user/conf/init_db.sh
@@ -2,9 +2,9 @@
 
 VAULT_ADDR="http://localhost:8200"
 SECRET_PATH="user_db"
-VAULT_TOKEN=$(cat .key)
+VAULT_TOKEN=$(cat /tmp/.key)
 
-response=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/$SECRET_PATH")
+response=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" "$VAULT_ADDR/v1/kv/$SECRET_PATH")
 
 DB_USER=$(echo "$response" | jq -r '.data.db_username')
 DB_BASENAME=$(echo "$response" | jq -r '.data.db_name')
@@ -21,4 +21,20 @@ psql -c "CREATE DATABASE ${TEMPLATE_DB_BASENAME} OWNER ${TEMPLATE_DB_USER};"
 psql -c "GRANT ALL PRIVILEGES ON DATABASE ${TEMPLATE_DB_BASENAME} TO ${TEMPLATE_DB_USER};"
 psql -c "GRANT ALL PRIVILEGES ON SCHEMA public TO ${TEMPLATE_DB_USER};"
 EOF
-4f042be89ec0ea453c6644ae18e13f501e270781
+
+
+# curl -s --header "X-Vault-Token: hvs.dsUt3IEvhcsDIbtwntivY2xU" "http://localhost:8200/v1/kv/user_db
+# {
+#     "request_id": "87814caa-91c2-7122-daeb-390e3a760d9c",
+#     "lease_id": "",
+#     "renewable": false,
+#     "lease_duration": 604800,
+#     "data": {
+#         "db_name": "testbase",
+#         "db_password": "kingp4bl0",
+#         "db_username": "dbking"
+#     },
+#     "wrap_info": null,
+#     "warnings": null,
+#     "auth": null
+# }
