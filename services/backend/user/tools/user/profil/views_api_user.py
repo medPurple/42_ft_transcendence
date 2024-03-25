7b66093683f7d33e0da2e8b0e3b12ba97f9bcb22
Marie-Laure Vautrot <vautrot.marielaure@gmail.com>
2024-03-25 15:06:26 +0100

    fix: profil syntax python compilation

--- services/backend/user/tools/user/profil/views_api_user.py
+++ services/backend/user/tools/user/profil/views_api_user.py
@@ -17,7 +17,7 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 	# def get_object(self):
 	# return get_object_or_404(CustomUser, username=self.kwargs['pk'])
 
- 	@action(detail=False, methods=['get'])
+	@action(detail=False, methods=['get'])
 	def profile(self, request, *args, **kwargs):
 		"""
 		Retrieve method description
@@ -27,25 +27,25 @@ class CustomUserViewSet(viewsets.ModelViewSet):
 		return Response(serializer.data)
 
 
-	 @action(detail=False, methods=['post'])
-    def edit_profile(self, request, *args, **kwargs):
-        form = CustomProfileForm(request.data, instance=request.user)
-        if form.is_valid():
-            form.save()
-            return Response({'success': True}, status=201)
-        else:
-            return Response(form.errors, status=400)
+	@action(detail=False, methods=['post'])
+	def edit_profile(self, request, *args, **kwargs):
+		form = CustomProfileForm(request.data, instance=request.user)
+		if form.is_valid():
+			form.save()
+			return Response({'success': True}, status=201)
+		else:
+			return Response(form.errors, status=400)
 
-    @action(detail=False, methods=['post'])
-    def delete_account(self, request, *args, **kwargs):
-        user = request.user
-        if not isinstance(user, AnonymousUser):
-            try:
-                user.delete()
-            except CustomUser.DoesNotExist:
-                return Response({'error': 'User not found'}, status=404)
-            logout(request)
-            return Response({'success': True}, status=200)
-        else:
-            return Response({'error': 'User not authenticated'}, status=401)
+	@action(detail=False, methods=['post'])
+	def delete_account(self, request, *args, **kwargs):
+		user = request.user
+		if not isinstance(user, AnonymousUser):
+			try:
+				user.delete()
+			except CustomUser.DoesNotExist:
+				return Response({'error': 'User not found'}, status=404)
+			logout(request)
+			return Response({'success': True}, status=200)
+		else:
+			return Response({'error': 'User not authenticated'}, status=401)
 
