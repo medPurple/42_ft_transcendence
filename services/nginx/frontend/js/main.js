2f7341fcada69fdb156d4e0a37f66e002759a496
Alban TARDIF <atardif@paul-f4Ar5s6.clusters.42paris.fr>
2024-04-02 16:28:29 +0200

    feat: adding model pong3d for test purpose

--- services/nginx/frontend/js/main.js
+++ services/nginx/frontend/js/main.js
@@ -8,63 +8,67 @@ import gameService from "./views/gameService.js";
 import userService from "./views/user/userService.js";
 import register from "./views/user/register.js";
 import login from "./views/user/login.js";
+import { setup } from "./components/pong3d/pongLogic.js";
 
 
 
 
 // Define the routes
 const routes = {
-    '/': {
-        title: "Home", 
-        render: home
-    },
-    '/about': {
-        title: "About", 
-        render: about
-    },
-    '/contact': {
-        title: "Contact", 
-        render: contact
-    },
-    '/userService': {
-        title: "User Service",
-        render: userService
-    },
-    '/gameService': {
-        title: "Game Service",
-        render: gameService
-    },
-    '/register': {
-        title: "Register",
-        render: register
-    },
-    '/login': {
-        title: "Log In",
-        render: login
-    },
+  '/': {
+    title: "Home",
+    render: home
+  },
+  '/about': {
+    title: "About",
+    render: about
+  },
+  '/contact': {
+    title: "Contact",
+    render: contact
+  },
+  '/userService': {
+    title: "User Service",
+    render: userService
+  },
+  '/gameService': {
+    title: "Game Service",
+    render: gameService
+  },
+  '/register': {
+    title: "Register",
+    render: register
+  },
+  '/login': {
+    title: "Log In",
+    render: login
+  },
 };
 
 // Define the router function that will render the view based on the route path name and update the browser history state
 function router() {
-    let view = routes[location.pathname];
-    // define the header title
-    const pageTitle = "Transcendence";
-    if (view) {
-        document.title = pageTitle + " | " + view.title;
-        app.innerHTML = view.render();
-    } else {
-        history.replaceState("", "", "/");
-        router()
+  let view = routes[location.pathname];
+  // define the header title
+  const pageTitle = "Transcendence";
+  if (view) {
+    document.title = pageTitle + " | " + view.title;
+    app.innerHTML = view.render();
+    if ((view.render()).includes("pong-renderer")) {
+      setup();
     }
+  } else {
+    history.replaceState("", "", "/");
+    router()
+  }
 };
 
 // Handle navigation
 window.addEventListener("click", (e) => {
-    if (e.target.matches("[data-link]")) {
-        e.preventDefault();
-        history.pushState("", "", e.target.href);
-        router();
-    }
+  if (e.target.matches("[data-link]")) {
+    e.preventDefault();
+    history.pushState("", "", e.target.href);
+    router();
+  }
 });
 
 // Update router
