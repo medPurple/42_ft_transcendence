af5b378f64ef20b4fe6850db08ca8dbb9b653bb4
BuildTools <unconfigured@null.spigotmc.org>
2024-04-06 11:08:42 +0200

    merge user

--- services/nginx/frontend/js/components/matchmaking/matchmakingClass.js
+++ services/nginx/frontend/js/components/matchmaking/matchmakingClass.js
@@ -2,95 +2,95 @@ import Icookies from "../cookie/cookie.js"
 
 class Matchmaking {
 
-    getID(){
-        fetch('/api/token/authenticate/', {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': Icookies.getCookie('csrftoken'), // Get the CSRF token from the form data
-                'Authorization': 'Bearer ' + Icookies.getCookie('token')
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                 console.log("user id is : " + data.get);
-                 return 
-            } else {
-                // Display validation errors or any other error message
-                alert('No such user');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Handle API errors
-        });
-    }
-    
-    pongMatchmaking() {
-        id = getID();
-        fetch('/api/queue/', {
-            method: 'POST',
-            headers: {
-                'X-CSRFToken': Icookies.getCookie('csrftoken'), // Get the CSRF token from the form data
-                'Authorization': 'Bearer ' + Icookies.getCookie('token')
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                 console.log("user id is : " + data.get);
-                 return 
-            } else {
-                // Display validation errors or any other error message
-                alert('No such user');
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-            // Handle API errors
-        });
+	getID(){
+		fetch('/api/token/authenticate/', {
+			method: 'POST',
+			headers: {
+				'X-CSRFToken': Icookies.getCookie('csrftoken'), // Get the CSRF token from the form data
+				'Authorization': 'Bearer ' + Icookies.getCookie('token')
+			}
+		})
+		.then(response => response.json())
+		.then(data => {
+			if (data.success) {
+				console.log("user id is : " + data.get);
+				return 
+			} else {
+				// Display validation errors or any other error message
+				alert('No such user');
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+			// Handle API errors
+		});
+	}
+	
+	pongMatchmaking() {
+		id = getID();
+		fetch('/api/queue/', {
+			method: 'POST',
+			headers: {
+				'X-CSRFToken': Icookies.getCookie('csrftoken'),
+				'Authorization': 'Bearer ' + Icookies.getCookie('token')
+			},
+			body: JSON.stringify({
+				"userID": id,
+				"game": "pong_multiplayer"
+			})
+		})
+		.then(response => response.json())
+		.then(data => {
+			if (data.success) {
+				console.log("Added to queue for pong multiplayer");
+				return 
+			} else {
+				alert('No such user');
+			}
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+	}
 
-    }
+	tournamentMatchmaking() {
+		
+		// Implémentez la logique de matchmaking pour le tournoi ici
+	}
 
-    tournamentMatchmaking() {
-        
-        // Implémentez la logique de matchmaking pour le tournoi ici
-    }
-
-    pkmMatchmaking() {
-        // Implémentez la logique de matchmaking pour le pkm ici
-    }
+	pkmMatchmaking() {
+		// Implémentez la logique de matchmaking pour le pkm ici
+	}
 }
 
 // Pour utiliser cette classe, vous pouvez créer une nouvelle instance et appeler les méthodes appropriées
 class MatchmakingButtons {
-    constructor(matchmaking) {
-        this.matchmaking = matchmaking;
-    }
-    
-    insertButtons() {
-        const matchmakingbutton = document.createElement('div');
-        
-        const multipongbutton = document.createElement('button');
-        multipongbutton.innerText = 'Pong Matchmaking';
-        multipongbutton.addEventListener('click', () => this.matchmaking.pongMatchmaking());
-        
-        const tournapongbutton = document.createElement('button');
-        tournapongbutton.innerText = 'Tournament Matchmaking';
-        tournapongbutton.addEventListener('click', () => this.matchmaking.tournamentMatchmaking());
-        
-        const multipkmbuttoon = document.createElement('button');
-        multipkmbuttoon.innerText = 'PKM Matchmaking';
-        multipkmbuttoon.addEventListener('click', () => this.matchmaking.pkmMatchmaking());
-        
-        buttonContainer.appendChild(multipongbutton);
-        buttonContainer.appendChild(tournapongbutton);
-        buttonContainer.appendChild(multipkmbuttoon);
-        
-        document.body.appendChild(matchmakingbutton);
-    }
+	constructor(matchmaking) {
+		this.matchmaking = matchmaking;
+	}
+	
+	insertButtons() {
+		const matchmakingbutton = document.createElement('div');
+		
+		const multipongbutton = document.createElement('button');
+		multipongbutton.innerText = 'Pong Matchmaking';
+		multipongbutton.addEventListener('click', () => this.matchmaking.pongMatchmaking());
+		
+		const tournapongbutton = document.createElement('button');
+		tournapongbutton.innerText = 'Tournament Matchmaking';
+		tournapongbutton.addEventListener('click', () => this.matchmaking.tournamentMatchmaking());
+		
+		const multipkmbuttoon = document.createElement('button');
+		multipkmbuttoon.innerText = 'PKM Matchmaking';
+		multipkmbuttoon.addEventListener('click', () => this.matchmaking.pkmMatchmaking());
+		
+		matchmakingbutton.appendChild(multipongbutton);
+		matchmakingbutton.appendChild(tournapongbutton);
+		matchmakingbutton.appendChild(multipkmbuttoon);
+		
+		document.body.appendChild(matchmakingbutton);
+	}
 }
 
 const matchmaking = new Matchmaking();
 const matchmakingButtons = new MatchmakingButtons(matchmaking);
-matchmakingButtons.insertButtons();
--- services/nginx/frontend/js/views/play.js
+++ services/nginx/frontend/js/views/play.js
@@ -1,9 +1,6 @@
 
-import "../components/matchmaking/pongMultiplayer.js";
+import "../components/matchmaking/matchmakingClass.js";
 
 export default () => `
-    <h1>Play</h1>
-    <button type="submit" id=pong_multiplayer_queue class="button">Pong - 1v1</button>
-    <button type="submit" id=pong_tournament_queue class="button">Pong - Tournament</button>
-    <button type="submit" id=pkm_multiplayer_queue class="button">Pokemon - 1v1</button>
+        matchmakingButtons.insertButtons();
     `;
