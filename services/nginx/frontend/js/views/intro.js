55096446e888251b8382ed766d49e4cc7d8d12a8
Ana <a.colmenero@gmail.com>
2024-05-01 18:29:11 +0200

    feat: added images and assets. changed structure in frontend (paths to img affected!)

--- services/nginx/frontend/js/views/intro.js
+++ services/nginx/frontend/js/views/intro.js
@@ -1,8 +1,8 @@
 
 export default () => `
 	<div class="image-container">
-        <img src="./images/InvitationCard_Front.png" alt="Original Image" class="original-image">
-        <img src="./images/InvitationCard_Rev.png" alt="Hover Image" class="hover-image">
+        <img src="./images/Site/InvitationCard_Front.png" alt="Original Image" class="original-image">
+        <img src="./images/Site/InvitationCard_Rev.png" alt="Hover Image" class="hover-image">
     </div>
     <div class="buttons-container">
         <input type="text" placeholder="Enter Code" class="code-input">
