87701d38fc000daff78d428f2fb2f7d1382498cf
BuildTools <unconfigured@null.spigotmc.org>
2024-05-04 15:50:08 +0200

    fix: vault missing line

--- services/vault/init_vault.sh
+++ services/vault/init_vault.sh
@@ -22,7 +22,6 @@ if [ ! -f /vault/file/vault_init.txt ]; then
 	echo "[VAULT] Token creation"
 	vault token create -display-name="pokemap" > "/vault/file/pokemap_token.txt"
 	vault token create -display-name="chat" > "/vault/file/chat_token.txt"
-
 	vault token create -display-name="JWToken" > "/vault/file/JWToken_token.txt"
 	vault token create -display-name="matchmaking" > "/vault/file/matchmaking_token.txt"
 	vault token create -display-name="user" > "/vault/file/user_token.txt"
