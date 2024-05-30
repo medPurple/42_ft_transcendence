32204233d2a25b64b6e5e2bad4593f8cecb82fe1
Ana COLMENERO <ancolmen@paul-f4Ar5s5.clusters.42paris.fr>
2024-05-30 21:46:37 +0200

    fix: endgame img

--- services/nginx/frontend/js/views/pong3d/pongEnd.js
+++ services/nginx/frontend/js/views/pong3d/pongEnd.js
@@ -1,53 +1,29 @@
 
-// export default () => `
-// 	<div class="container mt-5">
-// 		<div class="row justify-content-center">
-// 			<div class="col-md-10">
-// 				<div class="#custom-endgame">
-// 					<img src="../../../images/Game/P1-WINS.jpeg" alt="Display Image">
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</div>
-// `
-
-// export default () => `
-// 	<div class="container mt-5">
-// 		<div class="row justify-content-center">
-// 			<div class="col-md-10">
-// 				<div class="#custom-endgame">
-// 					<img src="../../../images/Game/P2-WINS.jpeg" alt="Display Image">
-// 				</div>
-// 			</div>
-// 		</div>
-// 	</div>
-// 	`
-
 export default async function pongEnd(winner) {
 	console.log('WINNER in pongEnd: ', winner);
 
 	if (winner == 1)
 		return `
-		<div class="container mt-5">
-			<div class="row justify-content-center">
-				<div class="col-md-10">
-					<div class="#custom-endgame">
-						<img src="../../../images/Game/P1-WINS.jpeg" alt="Display Image">
+			<div class="container mt-5">
+				<div class="row justify-content-center">
+					<div class="col-md-8">
+						<div id="custom-endgame">
+							<img src="../images/Game/P1-WINS.jpeg" class="img-fluid" alt="Display Image">
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		`
 	else
 		return `
-		<div class="container mt-5">
-			<div class="row justify-content-center">
-				<div class="col-md-10">
-					<div class="#custom-endgame">
-						<img src="../../../images/Game/P2-WINS.jpeg" alt="Display Image">
+			<div class="container mt-5">
+				<div class="row justify-content-center">
+					<div class="col-md-8">
+						<div id="custom-endgame">
+							<img src="../images/Game/P2-WINS.jpeg" class="img-fluid" alt="Display Image">
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
 		`
 }
