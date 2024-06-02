5e3dcd821368696eb8dee5e66d2ab8736a8d7136
BuildTools <unconfigured@null.spigotmc.org>
2024-06-02 22:46:41 +0200

    feat: continue chat

--- services/nginx/frontend/js/components/chat/chatClass.js
+++ services/nginx/frontend/js/components/chat/chatClass.js
@@ -3,6 +3,23 @@ import Iuser from "../user/userInfo.js"
 import Ifriends from "../friends/friendsInfo.js";
 
 export class chat {
+
+	constructor(){
+		this.targetid = 1;
+	}
+
+	async getusername(data) {
+		let message = data.message;
+		let parts = message.split(':');
+		let id = parts[0].trim();
+
+		const users = await Iuser.getAllUsers();
+		console.warn(users);
+		let username = users.users.find(user => user.user_id === parseInt(id)).username;
+		return username;
+
+	}
+
 	createChatDiv() {
 		const chatDiv = document.createElement('div');
 		chatDiv.style.backgroundColor = 'lightgray';
@@ -61,9 +78,11 @@ export class chat {
 		input.id = 'messageInput';
 
 		const sendButton = document.createElement('button');
+		sendButton.id = 'sendButton';
 		sendButton.textContent = 'Send';
 
 		const inviteButton = document.createElement('button');
+		inviteButton.id = 'inviteButton';
 		inviteButton.textContent = 'Invite';
 
 		inputDiv.appendChild(input);
@@ -73,16 +92,25 @@ export class chat {
 		return inputDiv;
 	}
 
-	createTitleDiv() {
+	async createTitleDiv() {
+
+		const response = await Iuser.getAllUsers();
+		let user = response.users.find(user => user.user_id === parseInt(this.targetid));
+		console.warn(user)
+
 		const titleDiv = document.createElement('div');
 		titleDiv.id = 'titleDiv';
 		titleDiv.style.backgroundColor = 'yellow';
+		titleDiv.classList.add('d-flex', 'flex-row', 'justify-content-center', 'align-items-center', 'w-100'); // Ajout des classes Bootstrap
 
 		const titleElement = document.createElement('h1');
-		titleElement.textContent = "title";
+		titleElement.textContent = user.username;
+		titleElement.classList.add('text-center', 'font-italic', 'mr-3',); // Ajout des classes Bootstrap
 
 		const imageElement = document.createElement('img');
-		imageElement.src = "./images/Favicons/PH-01extra.png";
+		imageElement.src = `data:image/jpeg;base64,${user.profile_picture_data}`;
+		imageElement.classList.add('rounded-circle'); // Ajout des classes Bootstrap
+
 
 		titleDiv.appendChild(titleElement);
 		titleDiv.appendChild(imageElement);
@@ -90,14 +118,15 @@ export class chat {
 		return titleDiv;
 	}
 
-	createInteractionDiv() {
+	async createInteractionDiv() {
 		const interactiondiv = document.createElement('div');
+		interactiondiv.classList.add('interactionDiv');
 		interactiondiv.style.backgroundColor = 'purple';
 		interactiondiv.style.flex = '3'; // Ajoute la propriété flex
 
 		const messagesDiv = this.createMessagesDiv();
 		const inputDiv = this.createInputDiv();
-		const titleDiv = this.createTitleDiv();
+		const titleDiv = await this.createTitleDiv();
 
 		interactiondiv.appendChild(titleDiv);
 		interactiondiv.appendChild(messagesDiv);
@@ -106,17 +135,124 @@ export class chat {
 		return	interactiondiv;
 	}
 
+	createNonediv(){
+		const Nonediv = document.createElement('div');
+		Nonediv.style.flex = '3';
+		Nonediv.innerText = "Nobody selected"
+
+
+		return Nonediv;
+	}
+
 	async initChat() {
 		const chatDiv = this.createChatDiv();
 		
 
 		const usersDiv = await this.createUsersDiv();
-		const interactiondiv = this.createInteractionDiv();
-
+		
 		chatDiv.appendChild(usersDiv);
-		chatDiv.appendChild(interactiondiv);
+		if (this.targetid){
+			chatDiv.appendChild(await this.createInteractionDiv());
+			this.createChatDiv();
+		}
+		else
+			chatDiv.appendChild(this.createNonediv());
 
 		document.body.appendChild(chatDiv);
 		return chatDiv;
 	}
+
+	async createChat(){
+		let myid = await Iuser.getID()
+		let roomName = ''
+		const interactiondiv = document.querySelector('.interactionDiv');
+		const inputDiv = interactiondiv.querySelector('#inputDiv');
+		const input = inputDiv.querySelector('#messageInput');
+		const sendButton = inputDiv.querySelector('#sendButton');
+		const inviteButton = inputDiv.querySelector('#inviteButton');
+
+
+		if (myid > this.targetid)
+			roomName = myid + '_' + this.targetid;
+		else
+			roomName = this.targetid + '_' + myid;
+
+		chatSocket = new WebSocket(
+			'wss://' + window.location.host + '/ws/chat/'
+			+ roomName
+			+ '/'
+		);
+
+		chatSocket.onopen = function(e) {
+			console.log('Chat socket open');
+		}
+
+		chatSocket.onmessage = function(e) {
+			const data = JSON.parse(e.data);
+			const message = data['message'];
+			this.checkInvite(message);
+			this.addMessage(message);
+		};
+
+		chatSocket.onclose = function(e) {
+			console.error('Chat socket closed unexpectedly');
+		};
+
+		input.focus();
+		input.onkeyup = (e) => {
+			if (e.key === 'Enter') {
+				e.preventDefault();
+				sendButton.click();
+			}
+		};
+
+		sendButton.onclick = async (e) => {
+			const message = input.value;
+			const user_id = await Iuser.getID();
+			chatSocket.send(JSON.stringify({
+				'message': message,
+				'user_id': user_id
+			}));
+			input.value = '';
+		}
+
+		inviteButton.onclick = async (e) => {
+			const user_id = await Iuser.getID();
+			chatSocket.send(JSON.stringify({
+				'message': '@invite@',
+				'user_id': user_id
+			}));
+		}
+
+	}
+
+	async addMessage(data){
+		const messagediv = interactiondiv.querySelector('#messagesDiv');
+		console.log(data);
+		const username = await this.getusername(data)
+		const usernameColor = this.getRandomColor(); // Function to generate random color
+		let parts = data.message.split(':');
+		let message = parts[1].trim();
+		messagediv.innerHTML += `<span style="color: ${usernameColor};">${username}</span>: ${message}<br>`;
+	}
+
+	getRandomColor() {
+		const letters = '0123456789ABCDEF';
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += letters[Math.floor(Math.random() * 16)];
+		}
+		return color;
+	}
+
+	async checkInvite(){
+		let message = data.message;
+		let parts = message.split(':');
+		let id = parts[0].trim();
+		let user_message = parts[1].trim();
+		if (user_message === '@invite@'){
+			console.warn('INVITATION BY' + id);
+		}
+	}
+
 }
