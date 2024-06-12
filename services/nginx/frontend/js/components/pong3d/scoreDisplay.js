dfdcd3a02831265493bf6f59e98a3c669a8b0edc
Ana COLMENERO <ancolmen@bess-f2r5s10.clusters.42paris.fr>
2024-06-12 13:24:04 +0200

    fix: almost there

--- services/nginx/frontend/js/components/pong3d/scoreDisplay.js
+++ services/nginx/frontend/js/components/pong3d/scoreDisplay.js
@@ -1,14 +1,11 @@
 import { gameState, core } from './config.js'
 
 export async function deleteForm() {
-
 	const formDiv = document.getElementById('app-general-container');
 	formDiv.removeChild(formDiv.lastChild);
 }
 
 export async function displayScore() {
-
-	// var endgame = document.getElementById("pong-renderer");
 	var scoreDiv = document.getElementById("pong-score");
 	scoreDiv.classList.add("scoreboard");
 	var player1Name = "Player 1";
@@ -17,44 +14,18 @@ export async function displayScore() {
 	if (core.player1_user_name != 0)
 		player1Name = core.player1_user_name;
 	if (core.player2_user_name != 0)
-		player1Name = core.player2_user_name;
-
+		player2Name = core.player2_user_name;
 
 	scoreDiv.innerHTML = `
-	<div class="container mt-5">
-		<div class="row justify-content-center">
-			<div class="col-md-10">
-				<div class="mt-4">
-					<div class="row">
-						<div class="col-md-6">
-							<h3>${player1Name}</h3>
-								<div class="score" id="score1">${gameState.player1Score}</div>
-						</div>
-						<div class="col-md-6">
-							<h3>${player2Name}</h3>
-								<div class="score" id="score2">${gameState.player2Score}</div>
-						</div>
-					</div>
-				</div>
+		<div class="row" style="color: white;">
+			<div class="col-md-6">
+				<h3><strong>${player1Name}</strong></h3>
+				<div class="score" id="score1"><strong>${gameState.player1Score}</strong></div>
+			</div>
+			<div class="col-md-6">
+				<h3><strong>${player2Name}</strong></h3>
+				<div class="score" id="score2"><strong>${gameState.player2Score}</strong></div>
 			</div>
 		</div>
-	</div>
-	`
-
-	// if (gameState.player1Score == gameState.score_limit || gameState.player2Score == gameState.score_limit) {
-	// 	scoreDiv.innerHTML = '';
-	// 	if (gameState.player1Score == gameState.score_limit) {
-	// 		endgame.innerHTML = `
-	// 		<div id="custom-endgame">
-	// 			<img src="../../../images/Game/P1-WINS.jpeg" class="img-fluid" alt="Display Image">
-	// 		</div>
-	// 		`;
-	// 	}
-	// 	else
-	// 		endgame.innerHTML = `
-	// 		<div id="custom-endgame">
-	// 			<img src="../../../images/Game/P2-WINS.jpeg" class="img-fluid" alt="Display Image">
-	// 		</div>
-	// 		`;
-	// }
+	`;
 }
